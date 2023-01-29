8a6e889bcb1ddaa3564e2f9128462870f825fedb
Jovan Gómez <gocar.jovan@gmail.com>
2023-01-28 22:10:29 -0600

    bugfix: add amount prop

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,61 +6,71 @@ export const products = (): IProduct[] => {
             id: 1,
             name: "Beds",
             size: 1.2,
-            icon: 'bed'
+            icon: 'bed',
+            amount: 0
         },
         {
             id: 2,
             name: "Refrigerador",
             size: 1,
-            icon: 'refrigerator'
+            icon: 'refrigerator',
+            amount: 0
         },
         {
             id: 3,
             name: "Furniture",
             size: 0.5,
-            icon: 'furniture'
+            icon: 'furniture',
+            amount: 0
         },
         {
             id: 4,
             name: "Oven",
             size: 0.6,
-            icon: 'oven'
+            icon: 'oven',
+            amount: 0
         },
         {
             id: 5,
             name: "Sofa",
             size: 1.5,
-            icon: 'sofa'
+            icon: 'sofa',
+            amount: 0
         },
         {
             id: 6,
             name: "TV",
             size: 0.25,
-            icon: 'tv'
+            icon: 'tv',
+            amount: 0
         },
         {
             id: 7,
             name: "Washer-dryer",
             size: 0.5,
-            icon: 'washer-dryer'
+            icon: 'washer-dryer',
+            amount: 0
         },
         {
             id: 8,
             name: "Dining",
             size: 2,
-            icon: 'dining'
+            icon: 'dining',
+            amount: 0
         },
         {
             id: 9,
             name: "Desk",
             size: 0.75,
-            icon: 'desk'
+            icon: 'desk',
+            amount: 0
         },
         {
             id: 10,
             name: "Wardrobe",
             size: 3.2,
-            icon: 'wardrobe'
+            icon: 'wardrobe',
+            amount: 0
         },
     ]
 }
